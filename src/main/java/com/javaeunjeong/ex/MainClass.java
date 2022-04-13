444dd2db8d19d0c4ddc134c7c0b516169181555a
choieunheong <mjchoi17@hanmail.net>
2022-04-13 11:29:32 +0900

    7-2 di 연습

---
+++ src/main/java/com/javaeunjeong/ex/MainClass.java
@@ -0,0 +1,21 @@
+package com.javaeunjeong.ex;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		//컨테이너 생성
+		ctx.load("classpath:applicationCTX.xml");//컨테이너 설정
+		ctx.refresh();
+		
+		Student student = ctx.getBean("student",Student.class);
+		System.out.println(student.getName());//컨테이너 사용
+		System.out.println(student.getAge());//컨테이너 사용
+		
+		ctx.close();
+	}
+
+}
